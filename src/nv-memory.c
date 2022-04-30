167a3db9ed40273d4178d0ac2826f796da7f7142
nevergarden <nevergarden.fa@gmail.com>
2022-04-30 16:54:12 +0430

    add: mem_align test functions

--- src/nv-memory.c
+++ src/nv-memory.c
@@ -2,15 +2,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#if defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 200112L
+#define MEMALIGN(ptr, align, size) posix_memalign(ptr, align, size)
+#elif _WIN32
+// Source: https://stackoverflow.com/a/33696858
+#define MEMALIGN(p, a, s) (((*(p)) = _aligned_malloc((s), (a))), *(p) ?0 :errno)
+#else
+#error "ERROR: aligned malloc is not supported."
+#endif
+
 static inline uint32_t get_page_size() {
+#ifdef _WIN32
+  SYSTEM_INFO sysinfo;
+  GetSystemInfo(&sysinfo);
+  return (uint32_t) sysinfo.dwAllocationGranularity;
+#else
   return sysconf(_SC_PAGESIZE);
+#endif
 }
 
 nv_memory_status_t nv_malloc(size_t size, nv_memory_block_t * block) {
   if(size == 0)
     return bad_allocation;
 
-  if(posix_memalign(&block->data, get_page_size(), size))
+  if(MEMALIGN(&block->data, get_page_size(), size))
     return bad_allocation;
 
   block->size = size;
@@ -23,3 +42,5 @@ void nv_free(nv_memory_block_t * block) {
     block->size = 0;
   }
 }
+
+#undef MEMALIGN

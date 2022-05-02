806550c26fb7ec695da52edfcfc67d9976842314
nevergarden <nevergarden.fa@gmail.com>
2022-05-02 17:25:58 +0430

    fix: test of getting x from path of build

--- test/nv_texture_test.c
+++ test/nv_texture_test.c
@@ -5,8 +5,8 @@ int main() {
   nv_memory_block_t block;
   assert(nv_malloc(10*4096, &block) == 0);
   nv_asset_map_t *map = nv_init_asset_map(&block);
-  nv_texture_info_t tex = load_texture("./res/test_rgba.png", &block, map);
-  printf("%d\n", tex.x);
+  nv_texture_info_t tex = load_texture("test/test_rgba.png", &block, map);
+  assert(tex.x == 250);
   nv_free_asset_map(map);
   nv_free(&block);
 }
